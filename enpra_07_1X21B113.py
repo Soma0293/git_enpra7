daf617dc35a27005c93d693a049e9565184a8c68
Soma <thaumatrope@suou.waseda.jp>
2023-06-06 16:12:16 +0900

    introduce 'for' and 'if'

--- enpra_07_1X21B113.py
+++ enpra_07_1X21B113.py
@@ -2,3 +2,17 @@ import random
 
 N = random.randint(100,999)
 
+max = 5
+
+for i in range(0,max):
+    x = input ("3桁の数字を入力してください。")
+    X = int(x)
+
+    if X == N:
+        print("正解です。")
+    elif X > N:
+        print("不正解です。もっと小さい数字です。")
+    else:
+        print("不正解です。もっと大きい数字です。")
+
+
